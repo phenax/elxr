61bb1f0c8b109c66d48e678a4d2cf7cb29cf7efb
Akshay Nair <phenax5@gmail.com>
2022-01-08 20:31:33 +0530

    test: adds group eval

--- tests/eval.spec.ts
+++ tests/eval.spec.ts
@@ -5,15 +5,32 @@ import { jlog } from '../src/utils'
 import { find } from '../src/eval'
 
 describe('Eval', () => {
-  it('should do shit', () => {
+  it('basic evaluation', () => {
+    const list = [0, 1, '2', 3, [4], 5]
+
     const liexp: ListExpr = [
       none,
       [ Expr.AnyNumber(null), Expr.Truthy(null) ],
       none,
     ]
+    expect(find(liexp, list)).toEqual([1, 3, 5])
 
+    const liexp2: ListExpr = [
+      none,
+      [ Expr.Falsey(null), Expr.Truthy(null) ],
+      none,
+    ]
+    expect(find(liexp2, list)).toEqual([])
+  })
+
+  it('with groups', () => {
     const list = [0, 1, '2', 3, [4], 5]
 
+    const liexp: ListExpr = [
+      none,
+      [ Expr.AnyItem(null), Expr.Group({ exprs: [ Expr.AnyNumber(null), Expr.Truthy(null)] }) ],
+      none,
+    ]
     expect(find(liexp, list)).toEqual([1, 3, 5])
   })
 })
